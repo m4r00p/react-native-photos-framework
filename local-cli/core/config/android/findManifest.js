eb5e97022a9fbc0f3e11ffc5ace70491d52f27c5
Marek Pawlowski <marekapawlowski@gmail.com>
2019-04-17 15:45:59 +0200

    v1.0.9

--- local-cli/core/config/android/findManifest.js
+++ local-cli/core/config/android/findManifest.js
@@ -8,10 +8,11 @@ const path = require('path');
  * @return {String}
  */
 module.exports = function findManifest(folder) {
+  console.log('findManifest', folder);
   const manifestPath = glob.sync(path.join('**', 'AndroidManifest.xml'), {
     cwd: folder,
     ignore: ['node_modules/**', '**/build/**', 'Examples/**', 'examples/**'],
   })[0];
-
+  console.log('manifestPath', manifestPath);
   return manifestPath ? path.join(folder, manifestPath) : null;
 };
